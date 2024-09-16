c6afa3712383955e54134901052787679d24b2db
ATEFwagdy <121570381+ATEFwagdy@users.noreply.github.com>
2024-09-16 22:02:00 +0300

    Update appColor.dart
    update ui

--- lib/shared/appColor.dart
+++ lib/shared/appColor.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 class AppColor{
-  static const splashColor = Color(0xff9d51ca);
-  static const ButtonClick = Color(0xff544d80);
+  static const splashColor = Color(0xff72A98F);
+  static const ButtonClick = Color(0xff72A98F);
   static const whiteColor = Colors.white;
   static const blackColor = Colors.black;
-  static const onboardingContainer = Color(0xff544d80);
+  static const onboardingContainer = Color(0xffCBEF43);
   static const containerDivider = Color(0xffE2E2E2);
   static const ListTileColor = Color(0xff181725);
   static const iconColor = Colors.grey;
-  static const iconColor2 = Color(0xfffb5c1c);
+  static const iconColor2 = Color(0xffFF4500);
   static const borderColor = Color(0xffdfdfe8);
   static const borderColor2 = Color(0xffF2F3F2);
   static const ratingColor = Colors.amber;
